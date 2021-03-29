04229235c3466163f253f2ea83886adf004071e0
rain-123-bow <80871800+rain-123-bow@users.noreply.github.com>
2021-03-29 09:02:39 +0800

    Add files via upload

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("search/", views.search, name="search"),
+    path("no_match", views.no_match, name="no_match"),
+    path("create_newpage", views.create_newpage, name="create_newpage"),
+    path("create/", views.create, name="create"),
+    path("random_page", views.random_page, name="random_page"),
+    path('<str:name>', views.show, name="show")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render
+import random
+from . import util, urls
 
-from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def show(request, name):
+    return render(request, "encyclopedia/target.html", {
+        "entries": markdown2.markdown(util.get_entry(name))
+    })
+
+def search(request):
+    search_word = request.GET.get('wd', '')
+    try:
+        return render(request, "encyclopedia/target.html", {
+            "entries": markdown2.markdown(util.get_entry(search_word))
+        })
+    except:
+        return render(request, "encyclopedia/no_match.html")
+
+
+def no_match(request):
+    return render(request, "encyclopedia/no_match.html")
+
+def create_newpage(request):
+    return render(request, "encyclopedia/create_newpage.html")
+
+def create(request):
+    title_copy = request.GET.get('title', '')
+    content_copy = request.GET.get('content', '')
+    util.save_entry(title_copy, content_copy)
+    return render(request, "encyclopedia/success.html", {
+        "entries": markdown2.markdown(util.get_entry(title_copy))
+    })
+
+def random_page(request):
+    list1 = util.list_entries()
+    num = len(list1)
+    randomNum = random.randrange(num)-1
+    return render(request, "encyclopedia/target.html", {
+        "entries": markdown2.markdown(util.get_entry(list1[randomNum]))
+    })
